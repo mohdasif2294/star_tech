74a03778481b056f728a2323f9e8eeb317d6c59f
Mayur <mayur.rastogi@myntra.com>
2020-07-17 01:37:10 +0530

    Fixing list brands and size

--- hackerramp/script.js
+++ hackerramp/script.js
@@ -4,11 +4,13 @@ window.onload = function(){
 		var brand = document.getElementById('brand').value;
 		var size = document.getElementById('size').value;
 	//	var obj={}
+		var brands = brand.split(",");
+		var sizes = size.split(",");
 		chrome.storage.sync.get('profile',function(items) {
 			if (typeof items.profile === 'undefined'){
 				items.profile = {};
 			}
-			items.profile[category] = {"brands":brand, "size":size}
+			items.profile[category] = {"brands":brands, "size":sizes}
 			chrome.storage.sync.set(items, function() {
 				console.log('Data successfully saved to the storage!');
 			});
