ecd44e5b1d2164de92e58ac69bf4768e634518a2
Mayur <mayur.rastogi@myntra.com>
2020-07-17 00:03:57 +0530

    Merging tanya's code

--- hackerramp/script.js
+++ hackerramp/script.js
@@ -1,18 +1,30 @@
 window.onload = function(){
 	document.getElementById('save').onclick = function (){
-	var value = document.getElementById('saveLine').value;
-	//alert(value);
-	//console.log(value);
-	
-	chrome.storage.sync.set({'myLine': value},function(){
-	alert('Success!');
-	}
-	);
-	}
+		var category = document.getElementById('category').value;
+		var brand = document.getElementById('brand').value;
+		var size = document.getElementById('size').value;
+		//alert(value);
+		//alert(category);
+		var obj = {}
+		obj[category] = {"brands":brand, "size":size}
 
-	document.getElementById('get').onclick = function (){
-	chrome.storage.sync.get('myLine', function(data){
-	alert(data.myLine);
+​
+	chrome.storage.sync.set({'profile': obj},function(){
+		console.log("before alert");
+		alert('Success!');
+		console.log("here", obj);
 	});
+​
+		//var cat = chrome.storage.sync.get('profile');
+		//console.log("cat", cat);
+		//chrome.storage.sync.set({'brand': brand}, function(){console.log('brand done')});
+		//chrome.storage.sync.set({'size': size}, function(){console.log('size done')});
+	}
+​
+	document.getElementById('get').onclick = function (){
+		chrome.storage.sync.get('profile', function(data){
+			console.log("asd",data.profile);
+			alert(JSON.stringify(data['profile']));
+		});
 	}
 }
