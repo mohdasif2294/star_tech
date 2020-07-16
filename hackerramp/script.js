aaa29f023116dd11643fc2cce41587f0c9d7dad9
tanya_300067949 <tanya.sharma2@myntra.com>
2020-07-16 16:10:43 +0530

    first backend commit

---
+++ hackerramp/script.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+	document.getElementById('save').onclick = function (){
+	var value = document.getElementById('saveLine').value;
+	//alert(value);
+	//console.log(value);
+	
+	chrome.storage.sync.set({'myLine': value},function(){
+	alert('Success!');
+	}
+	);
+	}
+
+	document.getElementById('get').onclick = function (){
+	chrome.storage.sync.get('myLine', function(data){
+	alert(data.myLine);
+	});
+	}
+}
