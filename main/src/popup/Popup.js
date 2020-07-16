6d9445878bf67464a31d6523695d8f5875993827
Asif <mohammad.asif@myntra.com>
2020-07-16 18:35:35 +0530

    added fonts and flat-ui

--- main/src/popup/Popup.js
+++ main/src/popup/Popup.js
@@ -1,27 +1,17 @@
-import React from 'react';
-import './Popup.css';
-
-const TechStackLogos = () => {
-  return (
-    <div className="tech-logos">
-      <img className="logo" src="/img/react.svg" alt="ReactJS logo" title="ReactJS" />
-      <img className="logo" src="/img/webpack.svg" alt="Webpack logo" title="Webpack" />
-      <img className="logo" src="/img/eslint.svg" alt="ESLint logo" title="ESLint" />
-      <img className="logo" src="/img/jest.svg" alt="Jest logo" title="Jest" />
-    </div>
-  );
-};
+import React from "react";
+import "./Popup.css";
 
 const Popup = () => {
   return (
     <div className="popup">
-      <p className="popup-greet">Thanks for using <span className="brand">Modern extension Boilerplate</span></p>
-      <p className="stack-head">Made using :</p>
-      <TechStackLogos />
-      <p className="contrib-msg">We would love some of your help in making this boilerplate even better. <br /><a href="https://www.github.com/kryptokinght/react-extension-boilerplate" target="_blank">React Extension Boilerplate</a></p>
+      <Grid
+        container
+        direction="row-reverse"
+        justify="center"
+        alignItems="center"
+      ></Grid>
     </div>
   );
 };
-
 
 export default Popup;
