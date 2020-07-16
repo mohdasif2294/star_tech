1e19c80c6e4a839ded8727a1f0a0ad81f3a38945
tanya_300067949 <tanya.sharma2@myntra.com>
2020-07-17 00:07:57 +0530

    merging code

--- hackerramp/script.js
+++ hackerramp/script.js
@@ -1,18 +1,30 @@
 window.onload = function(){
 	document.getElementById('save').onclick = function (){
-	var value = document.getElementById('saveLine').value;
+	var category = document.getElementById('category').value;
+	var brand = document.getElementById('brand').value;
+	var size = document.getElementById('size').value;
 	//alert(value);
-	//console.log(value);
+	//alert(category);
+	var obj = {}
+	obj[category] = {"brands":brand, "size":size}
 	
-	chrome.storage.sync.set({'myLine': value},function(){
+
+	chrome.storage.sync.set({'profile': obj},function(){
+	console.log("before alert");
 	alert('Success!');
-	}
-	);
+	console.log("here", obj);
+	});
+
+	//var cat = chrome.storage.sync.get('profile');
+	//console.log("cat", cat);
+	//chrome.storage.sync.set({'brand': brand}, function(){console.log('brand done')});
+	//chrome.storage.sync.set({'size': size}, function(){console.log('size done')});
 	}
 
 	document.getElementById('get').onclick = function (){
-	chrome.storage.sync.get('myLine', function(data){
-	alert(data.myLine);
+	chrome.storage.sync.get('profile', function(data){
+	console.log("asd",data.profile);
+	alert(JSON.stringify(data['profile']));
 	});
 	}
 }
