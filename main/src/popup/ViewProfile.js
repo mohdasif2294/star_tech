acc3cf0e8108fdc53907936ff4a83f8eed1dcc8e
Asif <mohammad.asif@myntra.com>
2020-07-17 06:46:30 +0530

    added the viewProfile

--- main/src/popup/ViewProfile.js
+++ main/src/popup/ViewProfile.js
@@ -3,28 +3,44 @@ var createReactClass = require("create-react-class");
 import { Multiselect } from "multiselect-react-dropdown";
 import "./Popup.css";
 import "../css/bootstrap.min.css";
+import { Button, ButtonGroup } from "reactstrap";
+import { RadioGroup, RadioButton } from "react-radio-buttons";
 
 let ViewProfile = createReactClass({
   getInitialState: function() {
     return {
       selectedProfile: "",
-      profileList: ["shoes", "jeans", "shirts"],
+      profileList: [],
       profileDetails: {},
-      categoryoptions: [
-        { name: "Shirts", id: 1 },
-        { name: "Tshirts", id: 2 },
-        { name: "Jeans", id: 2 },
-        { name: "Shoes", id: 2 }
-      ],
-      brandoptions: [
-        { name: "Levis", id: 1 },
-        { name: "Nike", id: 1 },
-        { name: "Roadster", id: 1 },
-        { name: "Hrx", id: 1 }
-      ],
-      selectedValue: []
+      profilesMap: {},
+      isLoaded: false
     };
   },
+
+  fetchProfileList: function() {
+    let self = this;
+    chrome.storage.sync.get("profile", function(data) {
+      let list = [];
+      if (typeof data.profile === "undefined") {
+        console.log("Undefined profile");
+      } else {
+        Object.keys(data.profile).forEach((key, idx) => {
+          console.log("key", key, data.profile[key]);
+          list.push(key);
+        });
+
+        self.setState({
+          profileList: list,
+          profilesMap: data.profile
+        });
+      }
+    });
+  },
+
+  componentWillMount: function() {
+    this.fetchProfileList();
+  },
+
   onSelect: function(e) {
     if (e.target.value == "disabled") {
       return;
@@ -40,14 +56,55 @@ let ViewProfile = createReactClass({
   },
 
   fetchProfileData: function(profile) {
-    console.log("Profile", profile);
+    let self = this;
+    let profilesMap = self.state.profilesMap;
+    Object.keys(profilesMap).forEach((key, idx) => {
+      console.log("key", key, data.profile[key], profile);
+      if (key == profile) {
+        let profileDetails = {};
+
+        profileDetails["brandList"] = profilesMap[key]["brands"];
+        profileDetails["sizeList"] = profilesMap[key]["size"];
+        profileDetails["genderList"] = profilesMap[key]["gender"];
+        profileDetails["colorList"] = profilesMap[key]["color"];
+        self.setState({
+          profileDetails: profileDetails,
+          isLoaded: true
+        });
+      }
+    });
   },
 
   onRemoveProfile: function() {
-    console.log("no");
+    let self = this;
+    let selectedProfile = self.state.selectedProfile;
+
+    console.log("onRemoveProfile");
+    chrome.storage.sync.get("profile", function(items) {
+      if (typeof items.profile === "undefined") {
+        items.profile = {};
+      }
+      delete items.profile.selectedProfile;
+      console.log("Item profile", items);
+      chrome.storage.sync.set(items, function() {
+        console.log("Data successfully saved to the storage!");
+      });
+    });
+
+    self.setState({
+      isLoaded: false,
+      profileDetails: {},
+      selectedProfile: ""
+    });
   },
+
   render: function() {
     let self = this;
+    console.log(
+      "state: ",
+      self.state,
+      Object.keys(self.state.profileDetails).length
+    );
     return (
       <div className="row container-fluid margin-top">
         <div className="row container-fluid content">
@@ -70,59 +127,117 @@ let ViewProfile = createReactClass({
               })}
             </select>
           </div>
-          <div className="row container-fluid">
-            <div
-              className="col-md-4 col-lg-4 col-xs-offset-3 col-xs-6 margin-top"
-              style={{ textAlign: "center" }}
-            >
-              <button
-                style={{ backgroundColor: "#e74c3c" }}
-                className="btn submitBtnStyle"
-                onClick={self.onSubmit}
-              >
-                <svg
-                  style={{ marginRight: "8px" }}
-                  width="1em"
-                  height="1em"
-                  viewBox="0 0 16 16"
-                  class="bi bi-trash"
-                  fill="currentColor"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                  <path
-                    fillRule="evenodd"
-                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+        </div>
+        {Object.keys(self.state.profileDetails).length > 0 ? (
+          <div>
+            <div className="row container-fluid">
+              <div className="col-xs-3 fontBold">Gender</div>
+              <div className="col-xs-9">
+                <RadioGroup horizontal value="">
+                  {self.state.profileDetails["genderList"].map(function(
+                    gender,
+                    idx
+                  ) {
+                    console.log("GenderList", gender);
+                    return (
+                      <RadioButton value={gender} disabled>
+                        {gender}
+                      </RadioButton>
+                    );
+                  })}
+                </RadioGroup>
+              </div>
+            </div>
+
+            <div className="row container-fluid">
+              <div className="col-xs-3"> Brands: </div>
+              <div className="row container-fluid">
+                <div className="col-xs-12">
+                  <Multiselect
+                    selectedValues={self.state.profileDetails["brandList"]}
+                    displayValue="brands"
+                    displayValue="key"
                   />
-                </svg>
-                REMOVE
-              </button>
+                </div>
+              </div>
+            </div>
+
+            <div className="row container-fluid">
+              <div className="col-xs-3"> Size: </div>
+              <div className="row container-fluid">
+                <div className="col-xs-12">
+                  <ButtonGroup>
+                    {self.state.profileDetails["sizeList"].map(function(
+                      size,
+                      idx
+                    ) {
+                      return <Button key={idx}>{size}</Button>;
+                    })}
+                  </ButtonGroup>
+                </div>
+              </div>
+            </div>
+
+            <div className="row container-fluid">
+              <div className="col-xs-3"> Color: </div>
+              <div className="row container-fluid">
+                <div className="col-xs-12">
+                  <ButtonGroup>
+                    {self.state.profileDetails["colorList"].map(function(
+                      color,
+                      idx
+                    ) {
+                      return <Button key={idx}>{color}</Button>;
+                    })}
+                  </ButtonGroup>
+                </div>
+              </div>
+            </div>
+
+            <div className="row container-fluid">
+              <div
+                className="col-md-4 col-lg-4 col-xs-offset-3 col-xs-6 margin-top"
+                style={{ textAlign: "center" }}
+              >
+                <button
+                  style={{ backgroundColor: "#e74c3c" }}
+                  className="btn submitBtnStyle"
+                  onClick={self.onRemoveProfile}
+                >
+                  <svg
+                    style={{ marginRight: "8px" }}
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 16 16"
+                    class="bi bi-trash"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                    <path
+                      fillRule="evenodd"
+                      d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                    />
+                  </svg>
+                  REMOVE
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-        {self.state.profileDetails.length > 0 ? (
-          <div>
-            <div className="container-fluid">
-              <span style={{ fontFamily: "verdana" }}>Category</span>
-              <Multiselect
-                options={this.state.categoryoptions} // Options to display in the dropdown
-                selectedValues={this.state.selectedValue} // Preselected value to persist in dropdown
-                onSelect={this.onSelect} // Function will trigger on select event
-                onRemove={this.onRemove} // Function will trigger on remove event
-                displayValue="name" // Property name to display in the dropdown options
-              />
+        ) : self.state.isLoaded == true ? (
+          <div className="row container-fluid">
+            <div
+              className="col-xs-10 col-xs-offset-1"
+              style={{
+                color: "#808e9b",
+                fontSize: "15px",
+                marginTop: "150px",
+                textAlign: "center",
+                fontWeight: 500
+              }}
+            >
+              Profile details are not present.
             </div>
-            <div className="container-fluid">
-              <span style={{ fontFamily: "verdana" }}>Brands</span>
-              <Multiselect
-                options={this.state.brandoptions} // Options to display in the dropdown
-                selectedValues={this.state.selectedValue} // Preselected value to persist in dropdown
-                onSelect={this.onSelect} // Function will trigger on select event
-                onRemove={this.onRemove} // Function will trigger on remove event
-                displayValue="name" // Property name to display in the dropdown options
-              />
-            </div>
-            <button class="btn btn-embossed btn-danger">Embossed Button</button>
           </div>
         ) : null}
       </div>
